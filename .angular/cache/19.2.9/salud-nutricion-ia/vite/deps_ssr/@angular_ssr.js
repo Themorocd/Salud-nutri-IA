b084aa7b6820102a309584766b9979f04fed2bc6
Andres Jose Marin Pascualvaca <73909321+Themorocd@users.noreply.github.com>
2025-06-10 03:55:11 +0200

    Primer commit limpio

---
+++ .angular/cache/19.2.9/salud-nutricion-ia/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,41 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-G4KMPFSC.js";
+import "./chunk-KTUUL7DQ.js";
+import "./chunk-VCEETZR4.js";
+import "./chunk-SCWLWXTX.js";
+import "./chunk-EUOWZBAT.js";
+import "./chunk-SCHYATUO.js";
+import "./chunk-7BVU4VPN.js";
+import "./chunk-43EGNU4R.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# Salud-nutricion-ia
+=======
+# salud-nutricion-ia
+
+>>>>>>> 0319326a (Initial commit)
