da9f1c30686b8ba63b0059f51449e4cff29fc76a
Andres Jose Marin Pascualvaca <73909321+Themorocd@users.noreply.github.com>
2025-06-11 03:23:05 +0200

    .

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -11,13 +11,19 @@ import { LanguageService } from '../../services/language.service';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent {  idioma: 'es' | 'en' = 'es';
+export class LandingComponent {
+  idioma: 'es' | 'en' = 'es';
+
 
   constructor(private languageService: LanguageService) {
     this.languageService.idioma$.subscribe((idioma: 'es' | 'en') => this.idioma = idioma);
+
   }
+
+
 
   cambiarIdioma() {
     this.languageService.setIdioma(this.idioma === 'es' ? 'en' : 'es');
   }
 }
+
